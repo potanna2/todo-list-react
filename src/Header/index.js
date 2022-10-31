3a251a207556ff39baa633038faae464340df1e0
Anna Potasznikow <annapotasznikow@gmail.com>
2022-10-31 20:04:11 +0100

    to do list react without any interactions

---
+++ src/Header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = ({ title }) => (
+    <header className="header">
+        <h1 className="header__title">{title}</h1>
+    </header>
+);
+
+export default Header;
