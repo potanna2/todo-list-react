7207b38ee444c104f140b4324e529ad0397e625d
Anna Potasznikow <annapotasznikow@gmail.com>
2022-11-10 18:42:54 +0100

    formatting

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -5,14 +5,14 @@ const Tasks = ({ tasks, hideDoneTasks }) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li
-            className={`tasks__item${task.done && hideDoneTasks ? " tasks__item--hidden" : ""}`}>
+                className={`tasks__item${task.done && hideDoneTasks ? " tasks__item--hidden" : ""}`}>
                 <button className="tasks__button tasks__button--toggleDone">
-            {task.done ? "✓" : ""}
-            </button>
-            <span className={`tasks__content${ task.done ? " tasks__content--done" : ""}`}>
-            {task.content}
-            </span>
-            <button className="tasks__button tasks__button--remove">🗑</button>
+                    {task.done ? "✓" : ""}
+                </button>
+                <span className={`tasks__content${task.done ? " tasks__content--done" : ""}`}>
+                    {task.content}
+                </span>
+                <button className="tasks__button tasks__button--remove">🗑</button>
             </li>
         ))}
     </ul>
